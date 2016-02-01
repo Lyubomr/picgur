883c7f70c673e4350c6927ff5193d8c277c87f48
Lyubomr <s.lyubomyr@gmail.com>
2016-02-01 17:31:02 -0500

    Refactoring routes to use resources

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -36,6 +36,11 @@ class ImagesController < ApplicationController
     end
   end
 
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to images_url
+  end
 
   private
   def image_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   root 'images#index'
-  get 'images' => 'images#index'
-  get 'images/new' => 'images#new'
-  get 'images/:id/edit' => 'images#edit', as: 'edit_image'
-  patch 'images/:id' => 'images#update'
-  post 'images' => 'images#create'
-  get 'images/:id' => 'images#show', as: 'image'
+  resources :images
 end
